c7585ce73de2103750657d3e8b5decc1bb40d6c3
João Diniz <116961614+joaodiniz20@users.noreply.github.com>
2023-12-22 13:01:39 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,3 @@
-//Priodade:
-
-//Não é Priodade:
-//diminuir quantidade de linhas que estão repetidas
-//Fazer efeito de transparencia em pixel vazios ou apagados
-//Criar botão para ativar e desativar as bordas(internas)
-//Botão de apagar tudo
-
 let json = {};
 json.name = 'matriz';
 
@@ -19,6 +11,9 @@ var pixel = [[c1,c1,c1,c1],
 var colorPicker;
 var cor;
 
+var diamentroBotao = 3;
+var grid = true;
+
 function setup() {
   createCanvas(650, 450);
   colorPicker = createColorPicker('#000000');
@@ -28,111 +23,84 @@ function setup() {
 function draw() {
   background(220);
   cor = colorPicker.value()
-  //talvez dê pra colocar tudo num for ou de alguma formas mais eficiente depois
+  //Cria e altera os pixels
+  //stroke('#000000')
+  for(let i = 0; i < 4; i++){
+    for(let j = 0; j < 4; j++){
+      grindInterno(grid)
+      fill(pixel[i][j]);
+      square(x+(60*j), y+(60*i), 60);
+    }
+  }
   stroke('#000000')
-  
-  //linha 1
-  fill(pixel[0][0]);
-  square(x+(60*0), y, 60);
-  fill(pixel[0][1]);
-  square(x+(60*1), y, 60);
-  fill(pixel[0][2]);
-  square(x+(60*2), y, 60);
-  fill(pixel[0][3]);
-  square(x+(60*3), y, 60);
-  //linha 2
-  fill(pixel[1][0]);
-  square(x+(60*0), y+(60*1), 60);
-  fill(pixel[1][1]);
-  square(x+(60*1), y+(60*1), 60);
-  fill(pixel[1][2]);
-  square(x+(60*2), y+(60*1), 60);
-  fill(pixel[1][3]);
-  square(x+(60*3), y+(60*1), 60);
-  //linha 3
-  fill(pixel[2][0]);
-  square(x+(60*0), y+(60*2), 60);
-  fill(pixel[2][1]);
-  square(x+(60*1), y+(60*2), 60);
-  fill(pixel[2][2]);
-  square(x+(60*2), y+(60*2), 60);
-  fill(pixel[2][3]);
-  square(x+(60*3), y+(60*2), 60);
-  //linha 4
-  fill(pixel[3][0]);
-  square(x+(60*0), y+(60*3), 60);
-  fill(pixel[3][1]);
-  square(x+(60*1), y+(60*3), 60);
-  fill(pixel[3][2]);
-  square(x+(60*2), y+(60*3), 60);
-  fill(pixel[3][3]);
-  square(x+(60*3), y+(60*3), 60);
+  alteraPixels();
+  botoes();
 }
 
+function alteraPixels(){
+  if(mouseIsPressed === true){
+    for(let i = 0; i < 4; i++){
+      for(let j = 0; j < 4; j++){
+        if(mouseX >= x+(60*j) && mouseX <= x+(60*(j+1)) && mouseY >= y+(60*i) && mouseY <= y+(60*(i+1))){
+         pixel[i][j] = cor;
+        }
+      }
+    }
+  }
+}
 
 function mouseClicked() {
-  //linha 1
-  if(mouseX >= x+(60*0) && mouseX <= x+(60*1) && mouseY >= y+(60*0) && mouseY <= y+(60*1)){
-     pixel[0][0] = cor;
-    //cor[0] = ' ';
-    //alert(pixel[0][0]);
-  } else if(mouseX >= x+(60*1) && mouseX <= x+(60*2) && mouseY >= y+(60*0) && mouseY <= y+(60*1)) {
-    pixel[0][1] = cor;
-  } else if(mouseX >= x+(60*2) && mouseX <= x+(60*3) && mouseY >= y+(60*0) && mouseY <= y+(60*1)) {
-    pixel[0][2] = cor;
-  } else if(mouseX >= x+(60*3) && mouseX <= x+(60*4) && mouseY >= y+(60*0) && mouseY <= y+(60*1)) {
-    pixel[0][3] = cor;
+  //Apaga todos os pixels
+  if(mouseX > 70 && mouseX < 118 && mouseY > 270 && mouseY < 298){
+     for(let i = 0; i < 4; i++){
+       for(let j = 0; j < 4; j++){
+         pixel[i][j] = c1
+       }
+     }
   }
-  //linha 2
-  if(mouseX >= x+(60*0) && mouseX <= x+(60*1) && mouseY >= y+(60*1) && mouseY <= y+(60*2)){
-     pixel[1][0] = cor;
-  } else if(mouseX >= x+(60*1) && mouseX <= x+(60*2) && mouseY >= y+(60*1) && mouseY <= y+(60*2)) {
-    pixel[1][1] = cor;
-  } else if(mouseX >= x+(60*2) && mouseX <= x+(60*3) && mouseY >= y+(60*1) && mouseY <= y+(60*2)) {
-    pixel[1][2] = cor;
-  } else if(mouseX >= x+(60*3) && mouseX <= x+(60*4) && mouseY >= y+(60*1) && mouseY <= y+(60*2)) {
-    pixel[1][3] = cor;
+  //Salva/envia Json
+  if(mouseX > 70 && mouseX < 118 && mouseY > 310 && mouseY < 338){
+    salvarMatriz();
   }
-  //linha 3
-  if(mouseX >= x+(60*0) && mouseX <= x+(60*1) && mouseY >= y+(60*2) && mouseY <= y+(60*3)){
-     pixel[2][0] = cor;
-  } else if(mouseX >= x+(60*1) && mouseX <= x+(60*2) && mouseY >= y+(60*2) && mouseY <= y+(60*3)) {
-    pixel[2][1] = cor;
-  } else if(mouseX >= x+(60*2) && mouseX <= x+(60*3) && mouseY >= y+(60*2) && mouseY <= y+(60*3)) {
-    pixel[2][2] = cor;
-  } else if(mouseX >= x+(60*3) && mouseX <= x+(60*4) && mouseY >= y+(60*2) && mouseY <= y+(60*3)) {
-    pixel[2][3] = cor;
+  // liga/desliga grid
+  if(mouseX > 70 && mouseX < 118 && mouseY > 350 && mouseY < 378 && grid == true){
+    grid = false;
+  } else if((mouseX > 70 && mouseX < 118 && mouseY > 350 && mouseY < 378 && grid == false)){
+     grid = true;
   }
-  //linha 4
-  if(mouseX >= x+(60*0) && mouseX <= x+(60*1) && mouseY >= y+(60*3) && mouseY <= y+(60*4)){
-     pixel[3][0] = cor;
-  } else if(mouseX >= x+(60*1) && mouseX <= x+(60*2) && mouseY >= y+(60*3) && mouseY <= y+(60*4)) {
-    pixel[3][1] = cor
-  } else if(mouseX >= x+(60*2) && mouseX <= x+(60*3) && mouseY >= y+(60*3) && mouseY <= y+(60*4)) {
-    pixel[3][2] = cor;
-  } else if(mouseX >= x+(60*3) && mouseX <= x+(60*4) && mouseY >= y+(60*3) && mouseY <= y+(60*4)) {
-    pixel[3][3] = cor;
-  }
+  
 }
 
 function salvarMatriz(){
-  //cor = cor.slice(1);
-  json.b = [
-    ["0x"+pixel[0][0].slice(1), "0x"+pixel[0][1].slice(1), "0x"+pixel[0][2].slice(1), "0x"+pixel[0][3].slice(1)],
-    ["0x"+pixel[1][0].slice(1), "0x"+pixel[1][1].slice(1), "0x"+pixel[1][2].slice(1), "0x"+pixel[1][3].slice(1)],
-    ["0x"+pixel[2][0].slice(1), "0x"+pixel[2][1].slice(1), "0x"+pixel[2][2].slice(1), "0x"+pixel[2][3].slice(1)],
-    ["0x"+pixel[3][0].slice(1), "0x"+pixel[3][1].slice(1), "0x"+pixel[3][2].slice(1), "0x"+pixel[3][3].slice(1)]
-  ]
+  json.b = [[],[],[],[]]
+  
+  for(let i = 0; i < 4; i++){
+    for(let j = 0; j < 4; j++){
+      json.b[i][j] = "0x"+pixel[i][j].slice(1)
+    }
+  }
+  
   saveJSON(json, 'matriz.json');
 }
 
-function keyReleased(){
-    if (keyCode == 88) {
-    salvarMatriz();
-    }
+function botoes(){
+  //botão: apaga tudo
+  fill(255);
+  rect(70, 270, 48, 28, diamentroBotao, diamentroBotao, diamentroBotao, diamentroBotao);
+  
+  //botao: envia/baixa Json
+  fill(60);
+  rect(70, 310, 48, 28, diamentroBotao, diamentroBotao, diamentroBotao, diamentroBotao);
+  
+  //botão: liga/desliga grid
+  fill(60,80,200);
+  rect(70, 350, 48, 28, diamentroBotao, diamentroBotao, diamentroBotao, diamentroBotao);
 }
 
-
-  
-
-//https://developer.mozilla.org/pt-BR/docs/Learn/JavaScript/Objects/JSON
+function grindInterno(g){
+  if(g == true){
+    stroke('#000000');
+  } else {
+    noStroke();
+  }
+}
